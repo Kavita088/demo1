a34aa2fb88b13720641199ab386433cae5511a3d
kinjalpatil03 <patilkavita8888@gmail.com>
2021-08-11 11:01:13 +0530

    s

---
+++ exceptionhandling.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 10;
+            int divisor = 0;
+            try
+            {
+                int output = n1 / divisor;
+            }
+            catch (DivideByZeroException)
+            {
+                System.Console.WriteLine("not possible to divide by zero");
+            }
+            finally
+            {
+                System.Console.WriteLine("finally exception");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
